583ddd09f5b3dd2088bfadbd7f2898772c3a7829
kayze <Q7w8E9r6>
2016-11-06 23:02:21 +0800

    2016-11-06 named-view

---
+++ js/namedView.js
@@ -0,0 +1,43 @@
+/**
+ * Created by hayze on 2016/11/6.
+ */
+
+
+const one = { template: '<div>one</div>'  }
+const two = { template: '<div>two</div>'  }
+const three = { template: '<div>three</div>'  }
+
+const router = new VueRouter({
+    routes:[
+        {
+            path: '/',
+            components: {
+                default: one,
+                two: two,
+                three: three
+            }
+        }
+        ,
+        {
+            path: '/namedView',
+            components: {
+                default: two,
+                two: three,
+                three: one
+            }
+        }
+    ]
+    ,
+    watch: {
+        '$route' (to, from) {
+            const toDepth = to.path.split('/').length
+            const fromDepth = from.path.split('/').length
+            this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+        }
+    }
+})
+
+new Vue({
+    router,
+    el: '#app'
+})
